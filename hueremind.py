eb26cf7e8cd85e97a927f64c492d9c7f08144b8f
crhext <crhext@gmail.com>
2020-04-28 17:41:48 +0100

    updated to include error handling, and the script returns lights to original state after meeting completion

--- hueremind.py
+++ hueremind.py
@@ -8,12 +8,13 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from dateutil import parser
 import pytz
+import sys
 
 #phue python wrapper
 from phue import Bridge
 import logging
 import random
-logging.basicConfig
+logging.basicConfig()
 
 b = Bridge('192.168.0.5')
 
@@ -45,50 +46,143 @@ def main():
                 pickle.dump(creds, token)
 
         service = build('calendar', 'v3', credentials=creds)
-
-        while 1:
-            
-                # Call the Calendar API
-                now = datetime.datetime.utcnow()
-                then = now + datetime.timedelta(minutes=2)
-                
-                # Get first event occuring in next 2 min
-                print('Getting events in the next 2 mins')
-                events_result = service.events().list(calendarId='primary', timeMin=now.isoformat() + 'Z', timeMax=then.isoformat() + 'Z',
-                                                    maxResults=1, singleEvents=True,
-                                                    orderBy='startTime').execute()
-                events = events_result.get('items', [])
-                
-                # if no event exists, turn light off
-                if not events:
-                    print('No upcoming events found.')
+        
+        prev_on = b.get_light(1, 'on')
+        prev_colour = b.get_light(1, 'xy')
+        current_on = b.get_light(1, 'on')
+        current_colour = b.get_light(1, 'xy')
+        status = 'off'
+        upcoming_count = 0
+        ongoing_count = 0
+        off_count = 0
+        err_count = 0
+        
+        
+        while True:
+                try:
+                    #Get current light status
+                    current_on = b.get_light(1, 'on')
+                    current_colour = b.get_light(1, 'xy')
                     
-                    b.set_light(1, 'on', False)
-                 
-                #if event exists, get the start date/time of the event 
-                for event in events:
-                    start = event['start'].get('dateTime', event['start'].get('date'))
-                    print(start, event['summary'])
+                    # Get current time + time 2 min
+                    now = datetime.datetime.utcnow()
+                    then = now + datetime.timedelta(minutes=2)
                     
-                    #compare start time vs current time. First parsing into format that python will let us compare
-                    if start:
-                        event_start = parser.parse(start)
-                        current_time = pytz.utc.localize(datetime.datetime.utcnow())
+                    # Get first event occuring in next 2 min
+                    print('Getting events in the next 2 mins')
+                    events_result = service.events().list(calendarId='primary', timeMin=now.isoformat() + 'Z', timeMax=then.isoformat() + 'Z',
+                                                        maxResults=1, singleEvents=True,
+                                                        orderBy='startTime').execute()
+                    events = events_result.get('items', [])
+                    
+                    # if no event exists, return lights back to previous
+                    if not events:
+                        if status == 'ongoing' or status == 'upcoming':
+                            if prev_on:
+                                b.set_light(1, 'on', True)
+                                b.set_light(1, 'xy', prev_colour)
+                            else:
+                                b.set_light(1, 'on', False)
+                            status = 'off'
                         
-                        #if event is upcoming, set light orange
+                        ongoing_count = 0
+                        upcoming_count = 0
+                        off_count = off_count + 1
+                        
+                        print('-----')
+                        print('No upcoming events found.')
+                        print('status: ', status)
+                        print('prev on: ', prev_on)
+                        print('prev colour: ', prev_colour)
+                        print('current on: ', current_on)
+                        print('current colour: ', current_colour)
+                        print('opcoming count: ', upcoming_count)
+                        print('ongoing count: ', ongoing_count)
+                        print('off count: ', off_count)
+                        print('error count: ', err_count)
+                        print('-----')
+                            
+                    
+                    #if event exists, get the start date/time of the event
+                    for event in events:
+                        
+                        start = event['start'].get('dateTime', event['start'].get('date'))
+                        
+                        #First parse into format that python will let us compare
+                        
+                        event_start = parser.parse(start)
+                        current_time   = pytz.utc.localize(datetime.datetime.utcnow())
+                            
+                        # compare start time vs current time. if event is upcoming, get current light details, & set light orange
                         if current_time < event_start:
+                            if upcoming_count == 0:
+                                prev_on = current_on
+                                prev_colour = current_colour
+                                                      
+                            upcoming_count = upcoming_count + 1
+                            ongoing_count = 0
+                            off_count = 0
+                            status = 'upcoming'
+                            
+                            print('-----')
+                            print('Event: ',start, event['summary'])
+                            print('status: ', status)
+                            print('prev on: ', prev_on)
+                            print('prev colour: ', prev_colour)
+                            print('current on: ', current_on)
+                            print('current colour: ', current_colour)
+                            print('opcoming count: ', upcoming_count)
+                            print('ongoing count: ', ongoing_count)
+                            print('off count: ', off_count)
+                            print('error count: ', err_count)
+                            print('-----')
+                                
+                                
+                                
                             b.set_light(1, 'on', True)
                             b.set_light(1, 'xy', [0.6142, 0.3785])
                             b.set_light(1, 'bri', 127)
-                        
-                        #if event is upcoming, set light red
+                            
+                        #if event is ongoing, set light red
                         else:
+                            status = 'ongoing'
+                            upcoming_count = 0
+                            ongoing_count += 1
+                            off_count = 0
+                            
+                            print('-----')
+                            print('Event: ',start, event['summary'])
+                            print('status: ', status)
+                            print('prev on: ', prev_on)
+                            print('prev colour: ', prev_colour)
+                            print('current on: ', current_on)
+                            print('current colour: ', current_colour)
+                            print('opcoming count: ', upcoming_count)
+                            print('ongoing count: ', ongoing_count)
+                            print('off count: ', off_count)
+                            print('error count: ', err_count)
+                            print('-----')
+                                
                             b.set_light(1, 'on', True)
                             b.set_light(1, 'xy', [0.6915, 0.3083])
+                            
+                    #call api every 5 seconds
+                    time.sleep(10)
+                    err_count = 0 
+                except KeyboardInterrupt:
+                    print('Programme Stopped')
+                    break
+                except Exception as e:
+                    print('There was an error: ', e)
+                    err_count = err_count + 1
+                    print('error count: ', err_count)
+                    time.sleep(10)
+                    if err_count < 10:
+                        continue
+                    else:
+                        break
                         
-                #call api every 5 seconds
-                time.sleep(5)
-
+                   
 if __name__ == '__main__':
     main()
  
